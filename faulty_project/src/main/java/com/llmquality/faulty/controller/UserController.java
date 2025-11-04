6ba27c2758a9458d1850b39280e1dea391161f22
G1t-Happens <44774729+G1t-Happens@users.noreply.github.com>
2025-11-04 12:42:47 +0100

    add: Maintainability issue – create tight coupling between controller and service

--- faulty_project/src/main/java/com/llmquality/faulty/controller/UserController.java
+++ faulty_project/src/main/java/com/llmquality/faulty/controller/UserController.java
@@ -2,6 +2,7 @@ package com.llmquality.faulty.controller;
 
 import com.llmquality.faulty.dto.*;
 import com.llmquality.faulty.dto.validation.UserValidationGroups;
+import com.llmquality.faulty.service.UserServiceImpl;
 import com.llmquality.faulty.service.interfaces.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("${api.base-path}/users")
 public class UserController {
 
-    private final UserService userService;
+    private final UserServiceImpl userService;
 
 
     @Autowired
-    public UserController(final UserService userService) {
+    public UserController(final UserServiceImpl userService) {
         this.userService = userService;
     }
 
