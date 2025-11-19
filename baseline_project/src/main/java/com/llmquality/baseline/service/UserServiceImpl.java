640e75e27fe039ae8b1219b0be4420e6b8b343bf
G1t-Happens <44774729+G1t-Happens@users.noreply.github.com>
2025-11-19 17:45:06 +0100

    add: fault 2 - Functional Suitability

--- baseline_project/src/main/java/com/llmquality/baseline/service/UserServiceImpl.java
+++ baseline_project/src/main/java/com/llmquality/baseline/service/UserServiceImpl.java
@@ -154,7 +154,6 @@ public class UserServiceImpl implements UserService {
                     return new ResourceNotFoundException(USER, "id", id);
                 });
 
-        userRepository.delete(existingUserEntity);
         LOG.debug("<-- delete, user with id {} deleted", existingUserEntity.getId());
     }
 
